26f36d63da889d16ee00b355bf100f90f213a6b2
Sike999 <mpasicnik625@gmail.com>
2025-07-06 16:04:33 +0500

    LINGVO.zip

--- real/resources/views/test.blade.php
+++
@@ -1,9 +0,0 @@
-<title>TEST</title>
-<h1>test {{$id / 10}}</h1>
-<p> {{$cas}} </p>
-@foreach ($data as $one)
-    <div>
-        <h2>{{ $one->Model }}</h2>
-        <p>{{ $one->Price }}</p>
-    </div>
-@endforeach
